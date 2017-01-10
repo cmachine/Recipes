0391cb93ce9a3c595eaab698012333d666bbafe8
root <root@ip-172-31-1-86.us-west-2.compute.internal>
2017-01-10 05:47:40 +0000

    Now with Lettuce!

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class Recipe(Base):
   __tablename__ = 'recipe'
   id = Column(Integer, primary_key=True)
+  name = Column(String, unique=True)
+
+class Ingredient(Base):
+  __tablename__ = 'ingredient'
+  id = Column(Integer, primary_key=True)
   name = Column(String)
+  qty = Column(Float)
+  unit = Column(String)
+  recipe = Column(String, ForeignKey('recipe.name'))
 
 engine = create_engine('sqlite:///recipies.db', echo=True)
 Base.metadata.create_all(engine)
@@ -14,6 +22,8 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
-new_recipe = Recipe(name='Taco Salad')
-session.add(new_recipe)
+taco_salad = Recipe(name='Taco Salad')
+lettuce = Ingredient(name='Lettuce', qty=1, unit='head', recipe='Taco Salad')
+session.add(taco_salad)
+session.add(lettuce)
 session.commit()
